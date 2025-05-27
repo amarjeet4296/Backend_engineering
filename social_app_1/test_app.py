420cfe62bbbc43685e5297c40832b60c859dc575
amarjeet krishnan <63232182+amarjeet4296@users.noreply.github.com>
2025-05-27 17:22:16 +0530

    Delete social_app_1 directory

--- social_app_1/test_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Simple Test App")
-st.write("If you can see this, Streamlit is working correctly!")
-
-# Add a simple widget
-number = st.slider("Select a number", 0, 100, 50)
-st.write(f"You selected: {number}")
